aab1e578871519f631b199ac6efa83e451e82882
timgent <tim.gent@gmail.com>
2015-04-17 11:59:27 +0100

    now reads from the csv file for the immigration data

--- app/models/ImmigrationRepository.scala
+++ app/models/ImmigrationRepository.scala
@@ -4,11 +4,21 @@ import scala.io.Source
 
 object ImmigrationRepository extends ImmigrationRepository
 
-trait ImmigrationRepository {
-  val src = Source.fromFile("/etc/passwd")
-  
-  def readImmigrationData() = {
-    
+class ImmigrationRepository {
+  lazy val immigrationData = readImmigrationData()
+
+  val src = Source.fromFile("app/resources/front_end_data.csv").getLines()
+
+  def readImmigrationData(): List[ImmigrationData] = {
+    // assuming first line is a header
+    val headerLine = src.take(1).next
+
+    // processing remaining lines
+    val data = src.toList.map { line =>
+      val fields = line.split("\\|")
+      ImmigrationData(fields.head, fields.tail.head)
+    }
+    data
   }
 }
 
--- test/models/ImmigrationRepositorySpec.scala
+++ test/models/ImmigrationRepositorySpec.scala
@@ -3,5 +3,14 @@ package models
 import org.specs2.mutable.Specification
 
 class ImmigrationRepositorySpec extends Specification {
-  ""
+  val immigrationRepository = new ImmigrationRepository()
+  "readImmigrationData" should {
+    "read in the correct data from the file" in {
+      val data = immigrationRepository.readImmigrationData()
+
+      val firstLine = data.head
+      firstLine.midaId mustEqual "395"
+      firstLine.outcome mustEqual "GRANT I.L.R. (ESOL)"
+    }
+  }
 }
