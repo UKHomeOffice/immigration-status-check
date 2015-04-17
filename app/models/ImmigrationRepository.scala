fd98fadf8a972c02b2c1010ecf5c14643bde4624
timgent <tim.gent@gmail.com>
2015-04-17 13:06:43 +0100

    Updates to repo method to return both status and outcome

--- app/models/ImmigrationRepository.scala
+++ app/models/ImmigrationRepository.scala
@@ -1,25 +1,45 @@
 package models
 
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+
 import scala.io.Source
 
 object ImmigrationRepository extends ImmigrationRepository
 
 class ImmigrationRepository {
-  lazy val immigrationData = readImmigrationData()
 
-  val src = Source.fromFile("app/resources/front_end_data.csv").getLines()
-
-  def readImmigrationData(): List[ImmigrationData] = {
-    // assuming first line is a header
-    val headerLine = src.take(1).next
-
-    // processing remaining lines
-    val data = src.toList.map { line =>
+  val src = Source.fromFile("app/resources/front_end_data.csv").getLines().drop(1)
+  
+  def checkStatus(id: String): Option[ImmigrationData] = {
+    src.foreach{line =>
       val fields = line.split("\\|")
-      ImmigrationData(fields.head, fields.tail.head)
+      val formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm")
+      val immigrationData = ImmigrationData(fields.head, fields(1), fields(2), DateTime.parse(fields(3), formatter))
+      if (immigrationData.midaId == id) return Some(immigrationData)
     }
-    data
+    return None
   }
+  
+
+//  val immigrationData = readImmigrationData()
+//  def readImmigrationData(): List[ImmigrationData] = {
+//    // assuming first line is a header
+//    val headerLine = src.take(1).next
+//
+//    // processing remaining lines
+//    val data = src.toList.map { line =>
+//      val fields = line.split("\\|")
+//      ImmigrationData(fields.head, fields.tail.head)
+//    }
+//    data
+//  }
+  
+//  def checkStatus(id: String): Option[ImmigrationData] = {
+////    val test = immigrationData.filter(_.midaId == id).headOption
+////    test
+//    Some(ImmigrationData("1093461", "GRANT I.L.R. (KOL TEST)"))
+//  }
 }
 
-case class ImmigrationData(midaId: String, outcome: String)
+case class ImmigrationData(midaId: String, outcome: String, status: String, applicationDate: DateTime)
--- test/models/ImmigrationRepositorySpec.scala
+++ test/models/ImmigrationRepositorySpec.scala
@@ -4,13 +4,32 @@ import org.specs2.mutable.Specification
 
 class ImmigrationRepositorySpec extends Specification {
   val immigrationRepository = new ImmigrationRepository()
-  "readImmigrationData" should {
-    "read in the correct data from the file" in {
-      val data = immigrationRepository.readImmigrationData()
+  
+  
+  
+//  "readImmigrationData" should {
+//    "read in the correct data from the file" in {
+//      val data = immigrationRepository.readImmigrationData()
+//
+//      val firstLine = data.head
+//      firstLine.midaId mustEqual "395"
+//      firstLine.outcome mustEqual "GRANT I.L.R. (ESOL)"
+//    }
+//  }
+  
+//  def checkStatusAndOutcome(id: String, )
+  
+  "checkStatus" should {
+    "return the outcome associated with an existing application" in {
+      val dataOption = immigrationRepository.checkStatus("885191")
+      val data = dataOption.get
+      val outcome = data.outcome
+      val status = data.status
 
-      val firstLine = data.head
-      firstLine.midaId mustEqual "395"
-      firstLine.outcome mustEqual "GRANT I.L.R. (ESOL)"
+      status mustEqual "Decision Made"
+      outcome mustEqual "GRANT I.L.R. (KOL TEST)"
     }
+    
+    
   }
 }
