2fa260f750c10e3e1d80ab07c297088e681177ab
timgent <tim.gent@gmail.com>
2015-04-17 14:41:11 +0100

    First name and surname added to repository

--- app/models/ImmigrationRepository.scala
+++ app/models/ImmigrationRepository.scala
@@ -15,31 +15,11 @@ class ImmigrationRepository {
     src.foreach{line =>
       val fields = line.split("\\|")
       val formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm")
-      val immigrationData = ImmigrationData(fields.head, fields(1), fields(2), DateTime.parse(fields(3), formatter))
+      val immigrationData = ImmigrationData(fields.head, fields(1), fields(2), DateTime.parse(fields(3), formatter), fields(4), fields(5))
       if (immigrationData.midaId == id) return Some(immigrationData)
     }
     return None
   }
-  
-
-//  val immigrationData = readImmigrationData()
-//  def readImmigrationData(): List[ImmigrationData] = {
-//    // assuming first line is a header
-//    val headerLine = src.take(1).next
-//
-//    // processing remaining lines
-//    val data = src.toList.map { line =>
-//      val fields = line.split("\\|")
-//      ImmigrationData(fields.head, fields.tail.head)
-//    }
-//    data
-//  }
-  
-//  def checkStatus(id: String): Option[ImmigrationData] = {
-////    val test = immigrationData.filter(_.midaId == id).headOption
-////    test
-//    Some(ImmigrationData("1093461", "GRANT I.L.R. (KOL TEST)"))
-//  }
 }
 
-case class ImmigrationData(midaId: String, outcome: String, status: String, applicationDate: DateTime)
+case class ImmigrationData(midaId: String, outcome: String, status: String, applicationDate: DateTime, firstName: String, surname: String)
