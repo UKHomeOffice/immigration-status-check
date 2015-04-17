55f4b349d0c4074b501e333feb2660f87188f498
Jordan Hatch <jordan@jordanh.net>
2015-04-17 15:06:47 +0100

    Lookup the immigration data for a given ID

--- app/controllers/ImmigrationController.scala
+++ app/controllers/ImmigrationController.scala
@@ -2,17 +2,39 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.ImmigrationRepository
 
 object ImmigrationController extends Controller {
 
   def index = Action {
     Ok(views.html.enterDetails("Your new application is ready."))
   }
-  
-  
-  
-  def showStatus = Action {
-    Ok("This worked!!")
+
+  val applicationForm = Form(
+    tuple(
+      "surname" -> nonEmptyText,
+      "date_of_birth_day" -> number,
+      "date_of_birth_month" -> number,
+      "date_of_birth_year" -> number,
+      "reference" -> nonEmptyText
+    )
+  )
+
+  def showStatus = Action { implicit request =>
+    applicationForm.bindFromRequest.fold(
+      formWithErrors => {
+        Ok(s"Missing information: $formWithErrors")
+      },
+      application => {
+        val applicationReference = application._5
+        val applicationDetails = ImmigrationRepository.checkStatus(applicationReference)
+
+        Ok(s"This worked!! $applicationDetails.firstName")
+      }
+    )
   }
 
 }
